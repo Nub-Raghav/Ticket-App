e96b9a9f373fbdc235d6f8be5746e7e6961658b2
Raghav <raghavlathi0003@gmail.com>
2024-05-22 18:27:46 +0530

    changed page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,38 +1,45 @@
 import TicketCard from "./(components)/TicketCard";
 
-const getTicket = async () => {
+const getTickets = async () => {
   try {
     const res = await fetch("http://localhost:3000/api/tickets", {
       cache: "no-store",
     });
-    const data = res.json();
-    return data;
+    return res.json();
   } catch (error) {
     console.log("Failed to get tickets ");
   }
 };
 
 const Dashboard = async () => {
-  const { tickets } = await getTicket();
+  const data = await getTickets();
+
+  // Make sure we have tickets needed for production build.
+  if (!data.tickets) {
+    return <p>No tickets.</p>;
+  }
+
+  const tickets = data.tickets;
 
   const uniqueCategories = [
     ...new Set(tickets.map(({ category }) => category)),
   ];
   return (
-    <div className="p-3">
+    <div className="p-5">
       <div>
         {tickets &&
-          uniqueCategories.map((uniCat, catInd) => (
-            <div key={catInd}>
-              <div className="flex justify-center">
-                <h2> {uniCat} </h2>
-              </div>
-
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
                 {tickets
-                  .filter((eachTick) => eachTick.category === uniCat)
+                  .filter((ticket) => ticket.category === uniqueCategory)
                   .map((filteredTicket, _index) => (
-                    <TicketCard id={_index} key={_index} {...filteredTicket} />
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
                   ))}
               </div>
             </div>
